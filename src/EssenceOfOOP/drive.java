be2d7ec18b4ff4ee436c1e61dbe0c4035bc22ea5
doit-zero <131241458+doit-zero@users.noreply.github.com>
2024-01-18 16:35:36 +0900

    불변객체 공부

--- src/EssenceOfOOP/drive.java
+++ src/EssenceOfOOP/drive.java
@@ -40,5 +40,37 @@ public class drive {
         System.out.println("student2 hashCode: " + student2.hashCode());
         System.out.println("student3 hashCode: " + student3.hashCode());
         System.out.println("student4 hashCode: " + student4.hashCode());
+
+        Integer x = 3;
+        Integer y = 3;
+        int z = 3;
+        System.out.println(x==y); // 참조타입을 비교하기 때문에 객체의 주소값을 비교함 그래서 false
+        System.out.println(x==z); // 기본데이터 타입이므로 값을 비교하기 때문에 자동으로 언박싱한 값을 비교하기 때문에 true
+        System.out.println(y==z); // 기본데이터 타입이므로 값을 비교하기 때문에 자동으로 언박싱한 값을 비교하기 때문에 true
+        System.out.println(x.equals(y)); // 객체의 상태의 값을 비교해주기 때문에 true가 나오는 것이다.
+
+        String m = new String("테스트");
+        String n = new String("테스트");
+
+        System.out.println(m==n); // 참조 타입 변수가 가리키는 주소값들이 다르므로 false
+        System.out.println(m.equals(n)); // 참조 타입 변수가 가리키는 객체 상태의 값이 같으므로 true
+
+        String r = "테스트";
+        String t = "테스트";
+
+        System.out.println(r==t); // 참조 타입 변수가 가리키는 주소 값들이 같은! heap영역에 constant pool에 생성되었기 때문!
+        System.out.println(r.equals(t)); // 참조 타입 변수가 가리키는 객체 상태의 값이 같으므로 당연히 true
+
+        for(int i = 0; i <10; i++){
+            String stringTest = new String("안녕하세요");
+            System.out.println(i + " " + stringTest);
+        }
+
+        for(int i = 0; i <10; i++){
+            String stringTest ="안녕하세요";
+            System.out.println(i + " " + stringTest);
+        }
+
+
     }
 }
